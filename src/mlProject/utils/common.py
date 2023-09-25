829e8f65052fa93a5c88bb77fa467762b4413297
iamharkirat <hchahal1@asu.edu>
2023-09-25 14:27:52 -0700

    logging & utls

---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,22 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from mlProject import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError('yaml filw is empty')
+    except Exception as e:
+        raise e
