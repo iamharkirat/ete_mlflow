95e7a76131fc1a711dd610f0660dc70d5e0ea613
iamharkirat <hchahal1@asu.edu>
2023-09-26 15:55:00 -0700

    stage 03 complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = 'Data Ingestion Stage'
@@ -20,5 +21,15 @@ try:
     data_ingestion.main()
     logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX===============X')
 except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Data Transformation Stage'
+try:
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX===============X')
+except exception as e:
     logger.exception(e)
     raise e
